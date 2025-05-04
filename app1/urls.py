d35d114baf3bfe5a7426382528569edeb89368dd
shreeya2809 <142043593+shreeya2809@users.noreply.github.com>
2025-05-04 15:46:44 +0530

    Update urls.py

--- app1/urls.py
+++ app1/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import analyze_image  # Make sure this is imported
 
 urlpatterns = [
     path('home/', views.HomePage, name='home'),
@@ -8,7 +9,7 @@ urlpatterns = [
     path('Image/', views.image_analysis, name='image_analysis'),
     path('Video/', views.video_analysis, name='video_analysis'),
     path('upload-image/', views.image_upload, name='image_upload'),
-
     
+    # Add the new URL pattern for the analyze_image view
+    path('analyze/', analyze_image, name='analyze'),  # New path added
 ]
-
